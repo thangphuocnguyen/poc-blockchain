88c2e823cba761745bd48c6ae110538f7d135adc
Thang Nguyen <thang.nguyenphuoc@asnet.com.vn>
2018-08-01 11:15:57 +0700

    Optimize by: + Prefix all parameters with underscore + Add gas optimization for safeMul + Consider to change `assert` to `require` since require make more gas effiency, but still keeping this by follow the rationale from here (https://github.com/OpenZeppelin/openzeppelin-solidity/issues/565)

--- ERC20-token/contracts/SafeMath.sol
+++ ERC20-token/contracts/SafeMath.sol
@@ -4,20 +4,35 @@ pragma solidity ^0.4.24;
 // Safe maths
 // ----------------------------------------------------------------------------
 contract SafeMath {
-    function safeAdd(uint a, uint b) public pure returns (uint c) {
-        c = a + b;
-        require(c >= a);
+    function safeAdd(uint256 _a, uint256 _b) public pure returns (uint256 c) {
+        c = _a + _b;
+        assert(c >= _a);
+        return c;
     }
-    function safeSub(uint a, uint b) public pure returns (uint c) {
-        require(b <= a);
-        c = a - b;
+    
+    function safeSub(uint256 _a, uint256 _b) public pure returns (uint256) {
+        assert(_b <= _a);
+        return _a - _b;
     }
-    function safeMul(uint a, uint b) public pure returns (uint c) {
-        c = a * b;
-        require(a == 0 || c / a == b);
+    
+    function safeMul(uint256 _a, uint256 _b) public pure returns (uint c) {
+        // Gas optimization: Check zero _a is cheaper than asserting 'a' not being zero,
+        // Bypass checking zero _b since it is rare case and this gas optimation will lose
+        // in case to check it
+        if (_a == 0) {
+            return 0;
+        }
+        
+        c = _a * _b;
+        assert(c / _a == _b);
+        
+        return c;
     }
-    function safeDiv(uint a, uint b) public pure returns (uint c) {
-        require(b > 0);
-        c = a / b;
+    
+    function safeDiv(uint256 _a, uint256 _b) public pure returns (uint256) {
+        // assert(_b > 0); // Solidity automatically throws when dividing by 0
+        // uint256 c = _a / _b;
+        // assert(_a == _b * c + _a % _b); // There is no case in which this doesn't hold
+        return _a / _b;
     }
 }
