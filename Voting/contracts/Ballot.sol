64de77254aee0da681f9ed72b55c417a8811e91b
Thang Nguyen <thang.nguyenphuoc@asnet.com.vn>
2018-11-28 16:52:04 +0700

    Update sample with modifier apply require(), assert()

--- Voting/contracts/Ballot.sol
+++ Voting/contracts/Ballot.sol
@@ -1,27 +1,34 @@
-pragma solidity ^0.4.23;
-
+pragma solidity ^0.4.24;
 contract Ballot {
 
     struct Voter {
         uint weight;
         bool voted;
         uint8 vote;
+        address delegate;
     }
     struct Proposal {
         uint voteCount;
     }
-    enum Stage {Init, Reg, Vote, Done}
+    enum Stage {Init,Reg, Vote, Done}
     Stage public stage = Stage.Init;
     
     address chairperson;
     mapping(address => Voter) voters;
     Proposal[] proposals;
 
+    event votingCompleted();
     
-    uint startTime;   
+    uint startTime;
+    //modifiers
+    modifier validStage(Stage reqStage) { 
+        require(stage == reqStage, "Invalid stage!");
+        _;
+    }
+
 
     /// Create a new ballot with $(_numProposals) different proposals.
-    constructor(uint8 _numProposals) public {
+    constructor(uint8 _numProposals) public  {
         chairperson = msg.sender;
         voters[chairperson].weight = 2; // weight is 2 for testing purposes
         proposals.length = _numProposals;
@@ -31,46 +38,40 @@ contract Ballot {
 
     /// Give $(toVoter) the right to vote on this ballot.
     /// May only be called by $(chairperson).
-    function register(address toVoter) public {
-        if (stage != Stage.Reg) {return;}
-        if (msg.sender != chairperson || voters[toVoter].voted) {
-            return;
-        }
-        
+    function register(address toVoter) public validStage(Stage.Reg) {
+        //if (stage != Stage.Reg) {return;}
+        if (msg.sender != chairperson || voters[toVoter].voted) return;
         voters[toVoter].weight = 1;
         voters[toVoter].voted = false;
-        if (now > (startTime + 10 seconds)) {
+        if (now > (startTime + 30 seconds)) {
             stage = Stage.Vote; 
-            startTime = now; 
         }        
     }
 
     /// Give a single vote to proposal $(toProposal).
-    function vote(uint8 toProposal) public {
-        if (stage != Stage.Vote) {return;}
+    function vote(uint8 toProposal) public validStage(Stage.Vote)  {
+       // if (stage != Stage.Vote) {return;}
         Voter storage sender = voters[msg.sender];
-        if (sender.voted || toProposal >= proposals.length) {
-            return;
-        }
+        if (sender.voted || toProposal >= proposals.length) return;
         sender.voted = true;
         sender.vote = toProposal;   
         proposals[toProposal].voteCount += sender.weight;
-        if (now > (startTime + 10 seconds)) {
-            stage = Stage.Done;
+        if (now > (startTime + 30 seconds)) {
+            stage = Stage.Done; 
+            emit votingCompleted();
         }        
         
     }
 
-    function winningProposal() public view returns (uint8 _winningProposal) {
-        if (stage != Stage.Done) {
-            return;
-        }
+    function winningProposal() public validStage(Stage.Done) view returns (uint8 _winningProposal) {
+       //if(stage != Stage.Done) {return;}
         uint256 winningVoteCount = 0;
-        for (uint8 prop = 0; prop < proposals.length; prop++) {
+        for (uint8 prop = 0; prop < proposals.length; prop++)
             if (proposals[prop].voteCount > winningVoteCount) {
                 winningVoteCount = proposals[prop].voteCount;
                 _winningProposal = prop;
             }
-        }
+        assert (winningVoteCount > 0);
+
     }
 }
