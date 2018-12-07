92999e054f7eca83783598340c35f875755c3a43
Thang Nguyen <thang.nguyenphuoc@asnet.com.vn>
2018-12-07 17:30:31 +0700

    Add entrypoint to init genesis block

---
+++ ethereum-private-network/ops/eth-client/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+cd /root/api-app
+perl -pi -e "s/XXX/$(hostname)/g" app.json
+/usr/bin/pm2 start ./app.json
+sleep 3
+
+# Create a database that uses genesis block
+geth --datadir=$ETH_DATA_DIR init "/root/files/genesis.json"
+sleep 3
+
+BOOTSTRAP_IP=`getent hosts "$ETH_MONITORED_HOST" | cut -d" " -f1`
+
+GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
+
+echo "GETH_OPTS=========================================================="
+echo $GETH_OPTS
+
+geth $GETH_OPTS
