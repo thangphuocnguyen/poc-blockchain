f7eb64979adac94284223a3705df626bbf4fba03
Thang Nguyen <thang.nguyenphuoc@asnet.com.vn>
2018-12-14 18:15:12 +0700

    Update docker-compose on fixed the ipaddress of some core service since ethereum force to use real IP adress on enode URL

--- ethereum-private-network/ops/eth-client/start.sh
+++ ethereum-private-network/ops/eth-client/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd /root/api-app
-perl -pi -e "s/XXX/$(hostname)/g" app.json
+# perl -pi -e "s/XXX/$(hostname)/g" app.json
 /usr/bin/pm2 start ./app.json
 sleep 3
 
@@ -9,11 +9,18 @@ sleep 3
 geth --datadir=$ETH_DATA_DIR init "/root/files/genesis.json"
 sleep 3
 
-BOOTSTRAP_IP=`getent hosts "$ETH_MONITORED_HOST" | cut -d" " -f1`
+# BOOTSTRAP_IP='getent hosts "$ETH_MONITORED_HOST" | cut -d" " -f1'
 
-GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
+# GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
 
-echo "GETH_OPTS=========================================================="
-echo $GETH_OPTS
+# echo "GETH_OPTS=========================================================="
+# echo $GETH_OPTS
+# echo "GETH_OPTS=========================================================="
 
-geth $GETH_OPTS
+# echo $ETH_MONITORED_HOST
+echo "OPTs======================================================"
+echo "$@"
+echo "=========================================================="
+
+# geth $GETH_OPTS
+geth "$@"
